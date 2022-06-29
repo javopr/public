ecdf4f18590309c6526cf0a376e0eb1630a74962
Javo Mora <javopr@gmail.com>
2022-06-29 10:29:54 -0400

    Create stojoin.ps1

---
+++ stojoin.ps1
@@ -0,0 +1,8 @@
+# Change the execution policy to unblock importing AzFilesHybrid.psm1 module
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+
+# Navigate to where AzFilesHybrid is unzipped and stored and run to copy the files into your path
+.\CopyToPSPath.ps1 
+
+# Import AzFilesHybrid module
+Import-Module -Name AzFilesHybrid
